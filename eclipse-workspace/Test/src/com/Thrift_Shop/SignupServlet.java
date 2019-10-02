9887f6f4b09f01cf82217176318ddf261d2b3a6e
V4G4X <varun.gawande@gmail.com>
2019-10-02 07:07:04 +0530

    Fixed 404 on SignupServlet

--- eclipse-workspace/Test/src/com/Thrift_Shop/SignupServlet.java
+++ eclipse-workspace/Test/src/com/Thrift_Shop/SignupServlet.java
@@ -1,6 +1,5 @@
 package com.Thrift_Shop;
 
-
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -11,103 +10,105 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 /**
  * Servlet implementation class SignupServer
  */
-@WebServlet("/SignupServer")
+@WebServlet("/SignupServlet")
 public class SignupServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public SignupServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public SignupServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String fname=request.getParameter("fname");
-		String lname=request.getParameter("lname");
-		String phno1=request.getParameter("phno1");
-		String phno2=request.getParameter("phno2");
-		String emailid=request.getParameter("emailid");
-		String username=request.getParameter("uname");
-		String password1=request.getParameter("pwd1");
-		String password2=request.getParameter("pwd2");
-		String building=request.getParameter("bldg");
-		String city=request.getParameter("city");
-		String neighbourhood=request.getParameter("neighbour");
-		String pincode=request.getParameter("pincode");
+		String fname = request.getParameter("fname");
+		String lname = request.getParameter("lname");
+		String phno1 = request.getParameter("phno1");
+		String phno2 = request.getParameter("phno2");
+		String emailid = request.getParameter("emailid");
+		String username = request.getParameter("uname");
+		String password1 = request.getParameter("pwd1");
+		String password2 = request.getParameter("pwd2");
+		String building = request.getParameter("bldg");
+		String city = request.getParameter("city");
+		String neighbourhood = request.getParameter("neighbour");
+		String pincode = request.getParameter("pincode");
 		int uid = 0;
-		
-		if(password1.compareTo(password2)!=0){
+
+		if (password1.compareTo(password2) != 0) {
 			response.sendRedirect("errorSignup.jsp");
 			System.out.println("Passwords did not match");
 		}
-		if(!fname.isEmpty() && !lname.isEmpty() && !phno1.isEmpty() && !emailid.isEmpty() && !username.isEmpty() && !password1.isEmpty() && !building.isEmpty() && !city.isEmpty() && !neighbourhood.isEmpty() && !pincode.isEmpty()) {
+		if (!fname.isEmpty() && !lname.isEmpty() && !phno1.isEmpty() && !emailid.isEmpty() && !username.isEmpty()
+				&& !password1.isEmpty() && !building.isEmpty() && !city.isEmpty() && !neighbourhood.isEmpty()
+				&& !pincode.isEmpty()) {
 			try {
 				Connection con = DatabaseConnection.initializeDatabase();
-				
-				PreparedStatement pst = con.prepareStatement("insert into User(fname,lname,email_id,building,city,neighbourhood,pincode)values(?,?,?,?,?,?,?)");
-				pst.setString(1,fname);
-				pst.setString(2,lname);
-				pst.setString(3,emailid);
-				pst.setString(4,building);
-				pst.setString(5,city);
-				pst.setString(6,neighbourhood);
-				pst.setString(7,pincode);
+
+				PreparedStatement pst = con.prepareStatement(
+						"insert into User(fname,lname,email_id,building,city,neighbourhood,pincode)values(?,?,?,?,?,?,?)");
+				pst.setString(1, fname);
+				pst.setString(2, lname);
+				pst.setString(3, emailid);
+				pst.setString(4, building);
+				pst.setString(5, city);
+				pst.setString(6, neighbourhood);
+				pst.setString(7, pincode);
 				pst.executeUpdate();
-				
+
 				pst = con.prepareStatement("Select uid from User where email_id =?");
-				pst.setString(1,emailid);
-				ResultSet rs=pst.executeQuery();
+				pst.setString(1, emailid);
+				ResultSet rs = pst.executeQuery();
 				rs.next();
-				uid=rs.getInt("uid");
-				
-				pst =con.prepareStatement("insert into Phone(uid,phone)values(?,?)");
-				pst.setInt(1,uid);
-				pst.setString(2,phno1);
+				uid = rs.getInt("uid");
+
+				pst = con.prepareStatement("insert into Phone(uid,phone)values(?,?)");
+				pst.setInt(1, uid);
+				pst.setString(2, phno1);
 				pst.executeUpdate();
-				
-				if(!phno2.isEmpty()) {
-					pst =con.prepareStatement("insert into Phone(uid,phone)values(?,?)");
-					pst.setInt(1,uid);
-					pst.setString(2,phno2);
+
+				if (!phno2.isEmpty()) {
+					pst = con.prepareStatement("insert into Phone(uid,phone)values(?,?)");
+					pst.setInt(1, uid);
+					pst.setString(2, phno2);
 					pst.executeUpdate();
 				}
-				
-				pst =con.prepareStatement("insert into Login(uid,username,password)values(?,?,?)");
-				pst.setInt(1,uid);
-				pst.setString(2,username);
-				pst.setString(3,password1);
+
+				pst = con.prepareStatement("insert into Login(uid,username,password)values(?,?,?)");
+				pst.setInt(1, uid);
+				pst.setString(2, username);
+				pst.setString(3, password1);
 				pst.executeUpdate();
-				
+
 				response.sendRedirect("Login.jsp");
-				
-				
-			}catch (Exception e) {
+
+			} catch (Exception e) {
 				System.out.print(e);
-				e.printStackTrace(); 
+				e.printStackTrace();
 			}
 		}
-		
-		
-		
-		
+
 		doGet(request, response);
 	}
 
